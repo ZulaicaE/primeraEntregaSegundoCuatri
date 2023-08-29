8166c6c2aef3fd93c445be7fc0b01e4a37e414c2
ZulaicaE <zulaicaemiliano@gmail.com>
2023-08-29 04:58:04 -0300

    xd

--- parte1.ts
+++ parte1.ts
@@ -1,3 +1,43 @@
 /*
 Como formatie, tuve que vincular todo de nuevo, esto es una prueba de commit and push
 */
+
+class sistemaDeStock { 
+
+    private elementosDisponibles: string[];
+    private elementosVendidos: string[] ;
+
+    public constructor() {
+        this.elementosDisponibles = [];
+        this.elementosVendidos = [];
+    }
+
+    public agregaElemento(item: string): void {
+        this.elementosDisponibles.push(item);
+    }
+
+    public agregaElementoVendido(item: string): void {
+        this.elementosVendidos.push(item);
+    }
+
+    public obtenerElementosDisponibles(): string[] {
+        return this.elementosDisponibles;
+    }
+
+    public obtenerElementosVendidos(): string[] {
+        return this.elementosVendidos;
+    }
+}
+
+class productoKiosco {
+
+    private nombreProducto: string;
+
+    public constructor() {
+        this.nombreProducto = '';
+    }
+
+    public obtenerNombreProducto(): string {
+        return this.nombreProducto;
+    }
+}
