6f83cb835ea6934aed75dcd1ac24cd9c468b8e14
ZulaicaE <zulaicaemiliano@gmail.com>
2023-08-29 21:38:37 -0300

    Funcionando, falta mejorar y comentar.

--- parte1.js
+++ parte1.js
@@ -6,8 +6,25 @@ var sistemaDeStock = /** @class */ (function () {
     sistemaDeStock.prototype.agregaElemento = function (item) {
         this.elementosDisponibles.push(item);
     };
-    sistemaDeStock.prototype.agregaElementoVendido = function (item) {
-        this.elementosVendidos.push(item);
+    sistemaDeStock.prototype.venderProducto = function (nombre, cantidad) {
+        for (var i = 0; i < this.elementosDisponibles.length; i++) {
+            if (nombre == this.elementosDisponibles[i].obtenerNombreProducto()) {
+                if (this.elementosDisponibles[i].obtenerCantidadProducto() >= cantidad) {
+                    this.elementosDisponibles[i].reducirStock(cantidad);
+                    this.agregaElementoVendido(this.elementosDisponibles[i], cantidad);
+                }
+                else {
+                    console.log('No tenemos esa cantidad de producto');
+                }
+            }
+            else if (i > this.elementosDisponibles.length) {
+                console.log('No tenemos este producto');
+            }
+        }
+    };
+    sistemaDeStock.prototype.agregaElementoVendido = function (producto, cantidad) {
+        var productoVendido = new productoKiosco(producto.obtenerNombreProducto(), producto.obtenerPrecioProducto(), cantidad);
+        this.elementosVendidos.push(productoVendido);
     };
     sistemaDeStock.prototype.obtenerElementosDisponibles = function () {
         return this.elementosDisponibles;
@@ -29,10 +46,28 @@ var productoKiosco = /** @class */ (function () {
     productoKiosco.prototype.obtenerPrecioProducto = function () {
         return this.precioProducto;
     };
-    productoKiosco.prototype.obtenerCantidadProcuto = function () {
+    productoKiosco.prototype.obtenerCantidadProducto = function () {
         return this.cantidadProducto;
+    };
+    productoKiosco.prototype.reducirStock = function (cantidad) {
+        this.cantidadProducto = this.cantidadProducto - cantidad;
+    };
+    productoKiosco.prototype.cambiarStock = function (cantidad) {
+        this.cantidadProducto = cantidad;
     };
     return productoKiosco;
 }());
+var stockDeKiosquito = new sistemaDeStock();
+var arrayDeStock = [];
 var cigarrilosMarlboro = new productoKiosco('Marlboro', 850, 10);
-console.log(cigarrilosMarlboro);
+var alfajorGuaymallen = new productoKiosco('Guaymallen', 100, 20);
+var surtidosBagley = new productoKiosco('Surtidos Bagley', 600, 5);
+arrayDeStock.push(cigarrilosMarlboro);
+arrayDeStock.push(alfajorGuaymallen);
+arrayDeStock.push(surtidosBagley);
+for (var i = 0; i < arrayDeStock.length; i++) {
+    stockDeKiosquito.agregaElemento(arrayDeStock[i]);
+}
+console.log(stockDeKiosquito);
+stockDeKiosquito.venderProducto('Marlboro', 4);
+console.log(stockDeKiosquito);
--- parte1.ts
+++ parte1.ts
@@ -13,8 +13,24 @@ class sistemaDeStock {
         this.elementosDisponibles.push(item);
     }
 
-    public agregaElementoVendido(item: productoKiosco): void {
-        this.elementosVendidos.push(item);
+    public venderProducto(nombre: string, cantidad: number): void {
+        for (let i: number = 0; i < this.elementosDisponibles.length; i++) {
+            if (nombre == this.elementosDisponibles[i].obtenerNombreProducto()) {
+                if (this.elementosDisponibles[i].obtenerCantidadProducto() >= cantidad) {
+                    this.elementosDisponibles[i].reducirStock(cantidad);
+                    this.agregaElementoVendido(this.elementosDisponibles[i], cantidad)
+                } else {
+                    console.log('No tenemos esa cantidad de producto');
+                }
+            } else if (i > this.elementosDisponibles.length) {
+                console.log('No tenemos este producto');
+            }
+        }
+    }
+
+    public agregaElementoVendido(producto: productoKiosco, cantidad: number): void {
+        let productoVendido: productoKiosco = new productoKiosco(producto.obtenerNombreProducto(), producto.obtenerPrecioProducto(), cantidad);
+        this.elementosVendidos.push(productoVendido);
     }
 
     public obtenerElementosDisponibles(): productoKiosco[] {
@@ -46,16 +62,38 @@ class productoKiosco {
         return this.precioProducto;
     }
 
-    public obtenerCantidadProcuto(): number {
+    public obtenerCantidadProducto(): number {
         return this.cantidadProducto;
+    }
+
+    public reducirStock(cantidad: number): void {
+        this.cantidadProducto = this.cantidadProducto - cantidad;
+    }
+
+    public cambiarStock(cantidad: number): void {
+        this.cantidadProducto = cantidad;
     }
 }
 
-
 let stockDeKiosquito = new sistemaDeStock();
 
+let arrayDeStock: productoKiosco[] = [];
+
 let cigarrilosMarlboro = new productoKiosco('Marlboro', 850, 10);
+let alfajorGuaymallen = new productoKiosco('Guaymallen', 100, 20);
+let surtidosBagley = new productoKiosco('Surtidos Bagley', 600, 5);
 
-console.log(cigarrilosMarlboro);
+arrayDeStock.push(cigarrilosMarlboro);
+arrayDeStock.push(alfajorGuaymallen);
+arrayDeStock.push(surtidosBagley);
 
-stockDeKiosquito.agregaElemento(cigarrilosMarlboro);
+for (let i: number = 0; i < arrayDeStock.length; i++) {
+    stockDeKiosquito.agregaElemento(arrayDeStock[i]);
+}
+
+console.log(stockDeKiosquito);
+
+stockDeKiosquito.venderProducto('Marlboro', 4);
+
+console.log(stockDeKiosquito);
+
